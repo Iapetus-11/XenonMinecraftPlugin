ec19b7f7b6cb06f66032d7925368c691e54d0842
Milo Weinberg <iapetus011@gmail.com>
2021-12-06 16:08:56 -0500

    add base files

---
+++ src/main/java/global/minecraft/XenonMinecraftPlugin.java
@@ -0,0 +1,15 @@
+package global.minecraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class XenonMinecraftPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        var server = getServer();
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
