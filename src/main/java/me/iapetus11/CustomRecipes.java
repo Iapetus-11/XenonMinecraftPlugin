e4f43670950a6a310d60e83f5fb453ad254927e8
Milo Weinberg <iapetus011@gmail.com>
2021-12-15 14:41:46 -0500

    add crafting wool to string

--- src/main/java/me/iapetus11/CustomRecipes.java
+++ src/main/java/me/iapetus11/CustomRecipes.java
@@ -4,11 +4,15 @@ import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.FurnaceRecipe;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapelessRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CustomRecipes {
+    private static String[] recipeKeys = {"smelt_rotten_flesh", "craft_wool_to_string"};
+
     public static void registerRecipes(JavaPlugin plugin) {
         var server = plugin.getServer();
+
         var smeltRottenFlesh = new FurnaceRecipe(
                 new NamespacedKey(plugin, "smelt_rotten_flesh"),
                 new ItemStack(Material.LEATHER),
@@ -16,11 +20,20 @@ public class CustomRecipes {
                 0.25f,
                 20);
 
+        var craftWoolToString = new ShapelessRecipe(
+                new NamespacedKey(plugin, "craft_wool_to_string"),
+                new ItemStack(Material.STRING, 4));
+        craftWoolToString.addIngredient(Material.WHITE_WOOL);
+
         server.addRecipe(smeltRottenFlesh);
+        server.addRecipe(craftWoolToString);
     }
 
     public static void unregisterRecipes(JavaPlugin plugin) {
         var server = plugin.getServer();
-        server.removeRecipe(new NamespacedKey(plugin, "smelt_rotten_flesh"));
+
+        for (String key : recipeKeys) {
+            server.removeRecipe(new NamespacedKey(plugin, key));
+        }
     }
 }
